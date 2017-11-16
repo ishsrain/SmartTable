ab7a5fac54c7cf50cc92181011b4592e6cbf4779
ishsrain <ishsrain@h2kresearch.com>
2017-11-16 18:03:12 +0900

    Image view Dynamic Create ExSelect

--- app/src/main/java/com/example/ishsrain/smarttable/MainActivity.java
+++ app/src/main/java/com/example/ishsrain/smarttable/MainActivity.java
@@ -2,34 +2,75 @@ package com.example.ishsrain.smarttable;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.ImageView;
+import android.view.MotionEvent;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.Toast;
+import com.example.ishsrain.smarttable.TableImageView.SelectViewListner;
+import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+  implements SelectViewListner {
+
+  ArrayList<TableImageView> mSelect = new ArrayList<TableImageView>();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    String[] str = {"b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o"};
-    for(int j=0; j<str.length; j++) {
-      for (int i = 0; i < 10; i++) {
-        // Resource ID
-        String viewID = "imageView" + ((j+1)*10 + i + 1);
-        int resourceID = getResources().getIdentifier(viewID, "id", getPackageName());
+    int row = 15;
+    int col = 11;
 
-        // Image ID
-        String image1Name = str[j] + ((i * 2) + 1);
-        String image2Name = str[j] + ((i * 2) + 2);
+    // Table
+    TableLayout tableLayout = (TableLayout)findViewById(R.id.TableLayout);
+
+    for(int j=0; j<row; j++) {
+
+      // New Row
+      TableRow tableRow = new TableRow(this);
+
+      for (int i = 0; i < col; i++) {
+
+        // New ImageView
+        TableImageView imageView = new TableImageView(this);
+
+        // Find ImageID
+        String image1Name = "han" + (j * 22 + i * 2);
+        String image2Name = "han" + (j * 22 + (i * 2) + 1);
         int image1ID = getResources().getIdentifier(image1Name, "drawable", getPackageName());
         int image2ID = getResources().getIdentifier(image2Name, "drawable", getPackageName());
+        imageView.setNormalImageID(image1ID);
+        imageView.setSelectImageID(image2ID);
 
-        TableImageView imageView = (TableImageView) findViewById(resourceID);
+        // Set Image
         imageView.setImage(image1ID);
-        imageView.setSelectImage(image2ID);
+
+        // Add ImageView
+        tableRow.addView(imageView);
+
+        TableRow.LayoutParams params = (TableRow.LayoutParams) imageView.getLayoutParams();
+        params.width = 0;
+        params.weight = 1;
+        imageView.setAdjustViewBounds(true);
+        imageView.setPadding(2,2,2,2);
       }
+
+      // Add Row
+      tableLayout.addView(tableRow);
     }
+  }
+
+  public void selectView(TableImageView view) {
+
+    for(int i = 0; i<mSelect.size(); i++) {
+      mSelect.get(i).cancelImage();
+    }
+
+    if(!mSelect.isEmpty()) {
+      mSelect.clear();
+    }
+
+    mSelect.add(view);
   }
 }
--- app/src/main/java/com/example/ishsrain/smarttable/TableImageView.java
+++ app/src/main/java/com/example/ishsrain/smarttable/TableImageView.java
@@ -1,11 +1,16 @@
 package com.example.ishsrain.smarttable;
 
 import android.content.Context;
-import android.graphics.Canvas;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class TableImageView extends AppCompatImageView {
 
@@ -13,8 +18,23 @@ public class TableImageView extends AppCompatImageView {
   int mSelectImage = 0;
   boolean mSelect = false;
 
+  SelectViewListner mCallback;
+
   public TableImageView(Context context) {
     super(context);
+
+    mCallback = (SelectViewListner) context;
+
+    setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        if(!mSelect) {
+          selectImage();
+        } else {
+          cancelImage();
+        }
+      }
+    });
   }
 
   public TableImageView(Context context,
@@ -27,48 +47,43 @@ public class TableImageView extends AppCompatImageView {
 
   }
 
-  @Override
-  protected void onDraw(Canvas canvas) {
-    super.onDraw(canvas);
-
-    if(mSelectImage != 0 && mImage != 0) {
-      if (mSelect) {
-        setImageResource(mSelectImage);
-      } else {
-        setImageResource(mImage);
-      }
-    }
-  }
-
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-    boolean ret_val = super.onTouchEvent(event);
-
-    if(event.getAction() == MotionEvent.ACTION_DOWN) {
-      if(!mSelect) {
-        selectImage();
-      } else {
-        cancelImage();
-      }
-    }
-    return ret_val;
-  }
-
   void selectImage(){
-    setImageResource(mSelectImage);
+    setImage(mSelectImage);
     mSelect = true;
+    mCallback.selectView(this);
   }
 
   void cancelImage(){
-    setImageResource(mImage);
+    setImage(mImage);
     mSelect = false;
   }
 
-  void setImage(int resource){
+  void setNormalImageID(int resource){
     mImage = resource;
   }
 
-  void setSelectImage(int resource){
+  void setSelectImageID(int resource){
     mSelectImage = resource;
+  }
+
+  void setImage(int resource) {
+
+//    setImageResource(resource);
+
+    Bitmap bmp = BitmapFactory.decodeResource(getResources(), resource);
+    Bitmap resizeBitmap = Bitmap.createScaledBitmap(bmp, 100, 100, true);
+//    bmp.recycle();
+//    BitmapDrawable bitmapDrawable = (BitmapDrawable) getDrawable();
+//    if(bitmapDrawable != null) {
+//      Bitmap currentBitmap = ((BitmapDrawable) getDrawable()).getBitmap();
+//      if (currentBitmap != null) {
+//        currentBitmap.recycle();
+//      }
+//    }
+    setImageBitmap(resizeBitmap);
+  }
+
+  public interface SelectViewListner {
+    void selectView(TableImageView view);
   }
 }
